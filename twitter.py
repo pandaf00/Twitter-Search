8e04cecb2bae0e9b388e0090b0c3f1dc19bba20b
pandaf00 <pandaf00@safe-mail.net>
2014-09-13 14:58:59 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -7,16 +7,38 @@ Last Modified: September 2014
 Twitter: @pandaf00
 """
 
+import csv
+from bs4 import BeautifulSoup
 import urllib2
 import sys
-from bs4 import BeautifulSoup
+import time
+import os
 
 def Search():
     hashtag = sys.argv[1]
+    # twitter url to grep results from
+    print "Loading results please wait..."
+    os.system("cls")
+    time.sleep(2.0)
     page = urllib2.urlopen("http://www.twitter.com/hashtag/" + hashtag)
     soup = BeautifulSoup(page)
-    tweets = soup.findAll("li", "js-stream-item")
     
-    print tweets
+    # data to extract
+    tweet_content   = soup.find_all("p", "js-tweet-text")
+    tweet_timestamp = soup.find_all("a", "tweet-timestamp")
+    tweet_links     = soup.find_all("a", "js-details")
+
+    print tweet_timestamp + tweet
+
+def save_Results():
+    # define which results need to be saved in our .csv
+    authors     = []
+    timestamp   = []
+    posts       = []
+    hashtags    = []
 
 Search()
+    
+
+
+
